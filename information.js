94c4ef88d9692710fb4f0fecec93e07deab497c9
LeriNiazashvili <leriniaza17@gmail.com>
2024-12-07 14:34:16 +0400

    Add files via upload

---
+++ information.js
@@ -0,0 +1,108 @@
+var mrg = document.getElementById("mrg");
+var btn = document.getElementById("btn");
+
+
+btn.addEventListener('click' , () => {
+ 
+mrg.innerHTML = "მერაბ დვალიშვილი ვანის რაიონის სოფელ სულორში იზრდებოდა, სწავლობდა სულორის საშუალო სკოლაში, მოგვიანებით სწავლა განაგრძო თბილისში.[1][2][3][4] ახალგაზრდულ ასაკში დაიწყო ვარჯიში ქართულ ჭიდაობაში, სამბოსა და ძიუდოში. 21 წლის ასაკში საცხოვრებლად გადავიდა აშშ-ში და გახდა პროფესიონალური შერეული ორთაბრძოლების მებრძოლი. მისი მწვრთნელები არიან რეი ლონგო და მატ სერა."
+});
+
+
+
+let mrg2 = document.getElementById("xvicha")
+let btn2 = document.getElementById("btn2")
+
+
+btn2.addEventListener('click' ,   () =>     {
+
+mrg2.innerHTML ="ხვიჩა კვარაცხელია (დ. 12 თებერვალი, 2001, თბილისი, საქართველო) — ქართველი ფეხბურთელი. საქართველოს ეროვნული ნაკრებისა და საფეხბურთო კლუბ „ნაპოლის“ შემტევი ნახევარმცველი. 2024 წლის ევროპის ჩემპიონატის მონაწილე საქართველოს ეროვნული ნაკრების შემადგენლობაში"
+
+
+
+});
+
+
+let mrg3 = document.getElementById("mrg3")
+let btn4 = document.getElementById("btn4")
+
+btn4.addEventListener('click' , () =>  {
+
+mrg3.innerHTML="გენო პეტრიაშვილი (დ. 1 აპრილი, 1994, გორი[1][2]) — ქართველი თავისუფალი სტილით მოჭიდავე. 2024 წლის პარიზის ოლიმპიური თამაშების ჩემპიონი,[3] მანამდე 2016 წლის რიო-დე-ჟანეიროს ოლიმპიური თამაშების ბრინჯაოსა და 2020 წლის ტოკიოს ოლიმპიური თამაშების ვერცხლის პრიზიორი. ევროპის 2-გზის ჩემპიონი (2016, 2020), 3-გზის ბრინჯაოსა (2013, 2017, 2021) და 2-გზის ვერცხლის (2018, 2019) პრიზიორი; ასევე 3-გზის მსოფლიოს ჩემპიონი (2017, 2018, 2019), 2-გზის ბრინჯაოსა (2013, 2015) და ვერცხლის (2021) პრიზიორი. მსოფლიოსა და ევროპის ჩემპიონი ახალგაზრდებს შორის (120 კგ.)"
+
+});
+
+
+
+let ilia = document.getElementById("mrg2")
+let iliabtn=document.getElementById("btn3")
+
+iliabtn.addEventListener('click' , () =>  {
+
+ilia.innerHTML="ილია თოფურია (დ. 21 იანვარი, 1997, ჰალე, გერმანია) — ქართველი პროფესიონალი მებრძოლი. ამჟამად ასპარეზობს ქვემსუბუქი წონითი დივიზიონის „აბსოლუტურ საბრძოლო ჩემპიონატში“ (Ultimate Fighting Championship, UFC). 2024 წლის 18 თებერვალს, ალექსანდრე ვოლკანოვსკის დამარცხების შემდეგ, ილია თოფურია გახდა UFC-ის ჩემპიონი."
+
+});
+
+
+
+
+
+let lashabtn = document.getElementById("lashabtn")
+let  lasha = document.getElementById("lasha")
+
+
+
+lashabtn.addEventListener('click' , ()  =>   {
+
+lasha.innerHTML="ლაშა გურული (დ. 27 აგვისტო, 1996) — ქართველი მოკრივე. მსოფლიოსა და ევროპის პრიზიორი. საერთაშორისო ტურნირის სამგზის ჩემპიონი (ბელარუსი, 2018, 64 კგ; საქართველო, 2018 და 2019, 64 კგ). ახალგაზრდებს შორის ევროპის ჩემპიონი (რუმინეთი, 2018, 64 კგ). 2024 წლის პარიზის ზაფხულის ოლიმპიური თამაშების ბრინჯაოს მედალოსანი."
+
+});
+
+
+let gigabtn = document.getElementById("gigabtn")
+let giga = document.getElementById("giga")
+
+
+
+gigabtn.addEventListener('click' , ()  => {
+
+giga.innerHTML="იგა ჭიკაძე (დ. 25 აგვისტო, 1988, თბილისი) — ქართველი კიკბოქსიორი და შერეული საბრძოლო ხელოვნების (MMA) მებრძოლი, რომელიც ამჟამად იბრძვის აბსოლუტურ საბრძოლო ჩემპიონატში (UFC). კიკბოქსინგში ჩატარებული 44 შეხვედრიდან მოგებული აქვს 38 (მათ შორის 22 ნოკაუტით)."
+
+});
+
+
+
+let children = document.getElementById("children")
+let now = document.getElementById("now")
+
+now.addEventListener('click' , ()  => {
+
+
+});
+
+
+var btn10 = document.getElementById("now")
+
+
+btn10.addEventListener('click', () => {
+
+var div = document.getElementById('children');
+
+div.style.width = "500px";
+div.style.height = "500px";
+div.style.borderRadius = "50%";
+div.style.backgroundColor = "red";
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
